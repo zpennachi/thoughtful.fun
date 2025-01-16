fce3fd661856c4d002dce41868e2a79006cd5b7a
zack pennachi <95557128+zpennachi@users.noreply.github.com>
2025-01-16 13:08:24 -0500

    Update app.js

--- app.js
+++ app.js
@@ -354,6 +354,7 @@ entry.file.forEach((fileUrl) => {
         modelViewer.cameraControls = true;
         modelViewer.style.position = 'relative';
       modelViewer.setAttribute('interaction-prompt', 'none');
+            modelViewer.setAttribute('touch-action', 'pan-y');
         modelViewer.style.top = '0';
         modelViewer.style.left = '0';
         modelViewer.style.width = '100%';
